dcee67bf5f14f93794a001f902570ee3a0a1e37d
Mozahid Rana <mozahidpic@gmail.com>
2024-05-21 17:23:07 +1000

    home page done

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,26 @@
+
 import { Button } from "@/components/ui/button";
+import { Jost } from "next/font/google";
+import { cn } from "@/lib/utils";
+import { LoginButton } from "@/components/auth/login-button";
+
+const font = Jost({
+  subsets: ["latin"],
+  weight: "600"
+});
 
 export default function Home() {
   return (
-    <Button size="lg" variant="outline">Click</Button>
+    <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+      <div className="space-y-6">
+        <h1 className={cn("text-6xl font-semibold text-center text-white drop-shadow-md", font.className)}>Auth</h1>
+        <p className="text-white text-lg">A simple authentication service</p>
+        <div>
+          <LoginButton>
+          <Button variant="secondary" size="lg">Sign In</Button>
+          </LoginButton>
+        </div>
+      </div>
+    </main>
   );
 }
