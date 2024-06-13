e5a7a1c2c2d558b52367515240d980695f64afab
Mozahid Rana <mozahidpic@gmail.com>
2024-06-13 12:38:50 +1000

    server component added

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,6 +1,3 @@
-'use client';
-
-import { useCurrentUser } from "@/hooks/use-current-user";
 import Navbar from "./_components/navbar";
 
 interface ProtectedLayoutProps {
@@ -8,11 +5,7 @@ interface ProtectedLayoutProps {
 }
 
 const ProtectedLayout = ({ children }: ProtectedLayoutProps) => {
-  const user = useCurrentUser();
-  if (!user) {
-    window.location.reload();
-    return null;
-  }
+
   return (
     <div className="h-full w-full flex flex-col gap-y-10 items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
       <Navbar />
